b99254e7a1d2539aea181fcb3aebef31c2003d55
Vishnu Rajeevan <vrajeevan@atlassian.com>
2019-03-11 14:18:19 -0400

    run deployment with `--info`

--- .buildscript/deploy_snapshot.sh
+++ .buildscript/deploy_snapshot.sh
@@ -21,6 +21,6 @@ elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
   echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
 else
   echo "Deploying snapshot..."
-  ./gradlew uploadArchives
+  ./gradlew --info uploadArchives
   echo "Snapshot deployed!"
 fi
