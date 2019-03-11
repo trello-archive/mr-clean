f87b4e7732d478e69b5cb2c7fdc32db950e5c101
Vishnu Rajeevan <vrajeevan@atlassian.com>
2019-03-11 13:00:20 -0400

    remove local copy of package-identifier

--- modules/package-identifier/package-identifier-annotations/src/main/java/com/trello/identifier/annotation/PackageIdentifier.kt
+++
@@ -1,4 +0,0 @@
-package com.trello.identifier.annotation
-
-@Target(AnnotationTarget.CLASS)
-annotation class PackageId(val isDebug: Boolean)
--- modules/package-identifier/package-identifier-plugin/src/main/java/com/trello/identifier/plugin/PackageIdentifierCompiler.kt
+++
@@ -1,5 +0,0 @@
-package com.trello.identifier.plugin
-
-interface PackageIdentifierCompiler {
-  fun createPackageIdentifierFile(packageName: String, output: Appendable, isDebuggable: Boolean)
-}
