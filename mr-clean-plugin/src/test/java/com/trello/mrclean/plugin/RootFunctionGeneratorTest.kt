94ad1c46f55170bdeba27a6753f7bd6c1d158eb0
Vishnu Rajeevan <vrajeevan@atlassian.com>
2019-08-07 14:42:37 -0400

    add test for reflection based root function

--- mr-clean-plugin/src/test/java/com/trello/mrclean/plugin/RootFunctionGeneratorTest.kt
+++ mr-clean-plugin/src/test/java/com/trello/mrclean/plugin/RootFunctionGeneratorTest.kt
@@ -7,7 +7,7 @@ import org.junit.Assert.*
 class RootFunctionGeneratorTest {
 
   @Test
-  fun createRootFunction() {
+  fun createRootFunctionWithoutReflection() {
     val generator = RootFunctionGenerator()
     val output = StringBuilder()
     generator.createRootFunction("com.example", false).writeTo(output)
@@ -21,6 +21,26 @@ class RootFunctionGeneratorTest {
 
       |internal fun Any.sanitizedToString(): String =
       |        error("No function generated! Make sure to annotate with @Sanitize")
+      |
+    """.trimMargin()
+    assertEquals(expectedOutput, output.toString())
+  }
+
+  @Test
+  fun createRootFunctionWithReflection() {
+    val generator = RootFunctionGenerator()
+    val output = StringBuilder()
+    generator.createRootFunction("com.example", true).writeTo(output)
+
+    val expectedOutput = """
+      |// This is the root function that generated functions will overload
+      |package com.example
+
+      |import com.trello.mrclean.reflect.reflectedToString
+      |import kotlin.Any
+      |import kotlin.String
+
+      |internal fun Any.sanitizedToString(): String = reflectedToString()
       |
     """.trimMargin()
     assertEquals(expectedOutput, output.toString())
