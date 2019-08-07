4765249b78e611b10e79ddb72de890cc8942e210
Vishnu Rajeevan <vrajeevan@atlassian.com>
2019-08-07 14:20:07 -0400

    Update function name and rendering
    Change the function name to reduce callsite confusion.
    
    Include class type name in output string.

--- mr-clean-reflect/src/main/java/com/trello/mrclean/reflect/Sanitizer.kt
+++ mr-clean-reflect/src/main/java/com/trello/mrclean/reflect/Sanitizer.kt
@@ -3,12 +3,12 @@ package com.trello.mrclean.reflect
 import kotlin.reflect.full.memberProperties
 import kotlin.reflect.jvm.javaGetter
 
-inline fun <reified T: Any> T.sanitizedToString(): String {
+inline fun <reified T: Any> T.reflectedToString(): String {
   val instance = this
   val sanitizedString = instance::class.memberProperties
       .joinToString {
         "${it.name} = ${it.javaGetter?.invoke(instance)}"
       }
-  return "{ $sanitizedString }"
+  return "${instance::class.simpleName}( $sanitizedString )"
 }
 
